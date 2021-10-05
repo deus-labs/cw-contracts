61cd0cdcb1600c8ecaeda2a49159af30d9655d25
Orkun Külçe <orkunkl@users.noreply.github.com>
2021-10-05 13:31:13 +0300

    Init cw20-pot

---
+++ contracts/cw20-pot/src/state.rs
@@ -0,0 +1,26 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{Addr, Uint128};
+use cw_storage_plus::Item;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub count: i32,
+    pub owner: Addr,
+}
+
+pub const STATE: Item<State> = Item::new("state");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub owner: Addr,
+}
+
+pub const CONFIG: Item<Config> = Item::new("config");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Pot {
+    pub target_addr: Addr,
+    pub threshold_amount: Uint128
+}
